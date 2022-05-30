785dcefa683cd8ab64e2042f0455cbbd2a1d4bff
SteelPlated <mix.singatulin@yandex.ru>
2022-05-30 23:02:53 +0500

    1

---
+++ HWSeminar6/task41/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 41. Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Введите элементы массива через пробел: ");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int count=0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i]>0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Кол-во элементов > 0: {count}");
